cafabccb809e0bee4bde229cca6f94343bdcbd2c
Apollo156156 <48605227+Apollo156156@users.noreply.github.com>
2020-01-20 17:37:34 +0400

    Добавила вёрстку на регистрацию и авторизацию

--- shopDress/resources/views/auth/register.blade.php
+++ shopDress/resources/views/auth/register.blade.php
@@ -1,18 +1,20 @@
 @extends('layouts.app')
 
-@section('content')
+@section('content') 
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Регистрация') }}</div>
+                <div class="card-header"></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="LastName" class="col-md-4 col-form-label text-md-right">{{ __('Фамилия') }}</label>
+                            <label for="LastName" class="col-md-4 col-form-label text-md-right">{{ __('SurName') }}</label>
 
                             <div class="col-md-6">
                                 <input id="LastName" type="text" class="form-control @error('LastName') is-invalid @enderror" name="LastName" value="{{ old('LastName') }}" required autocomplete="LastName" autofocus>
@@ -26,7 +28,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Имя') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -40,7 +42,7 @@
                         </div>
                         
                         <div class="form-group row">
-                            <label for="SecondName" class="col-md-4 col-form-label text-md-right">{{ __('Отчество') }}</label>
+                            <label for="SecondName" class="col-md-4 col-form-label text-md-right">{{ __('SecondName') }}</label>
 
                             <div class="col-md-6">
                                 <input id="SecondName" type="text" class="form-control @error('SecondName') is-invalid @enderror" name="SecondName" value="{{ old('SecondName') }}" required autocomplete="SecondName" autofocus>
@@ -54,7 +56,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Адрес почты(логин)') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -68,7 +70,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="Phone" class="col-md-4 col-form-label text-md-right">{{ __('Телефон') }}</label>
+                            <label for="Phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone number') }}</label>
 
                             <div class="col-md-6">
                                 <input id="Phone" type="tel" class="form-control @error('Phone') is-invalid @enderror" name="Phone" value="{{ old('Phone') }}" required autocomplete="Phone">
@@ -82,7 +84,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Пароль') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -96,7 +98,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Подтвердить пароль пароль') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Reset password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -106,7 +108,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Зарегестрироваться') }}
+                                    {{ __('Register') }}
                                 </button>
                             </div>
                         </div>
--- shopDress/resources/views/layouts/app.blade.php
+++ shopDress/resources/views/layouts/app.blade.php
@@ -17,18 +17,143 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet" type="text/css" media="all" />
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" media="all" />	
+    <link rel="stylesheet" href="{{ asset('css/jstarbox.css') }}" type="text/css" media="screen" charset="utf-8" />
+    <link href="{{ asset('css/style4.css') }}" rel="stylesheet" type="text/css" media="all" />	
+    <link rel="stylesheet" href="{{ asset('css/chocolat.css') }}" type="text/css" media="screen" charset="utf-8">  
+    <link href="{{ asset('css/test.css') }}" rel="stylesheet" type="text/css" media="all" />	
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<div class="header">
+<div class="container">
+		<div class="head">
+			<div class=" logo">
+				<a href="#"><img src="{{ asset('images/logo.png') }}" alt=""></a>	
+			</div>
+		</div>
+	</div>
+	<div class="header-top">
+		<div class="container">
+		<div class="col-sm-5 col-md-offset-2  header-login">
+			@if (!Auth::user())
+				<ul >
+					<li><a href="{{ route('login') }}">Login</a></li>
+					<li><a href="{{route('register')}}">Register</a></li>
+					<li><a href="checkout.html">Checkout</a></li>
+				</ul>
+			@else
+			<ul >
+					<li><a href="#">{{ Auth::user()->name }} </a></li>
+					<li><a href="checkout.html">Checkout</a></li>
+				</ul>
+
+			@endif
+
+
+				</div>
+				
+			<div class="col-sm-5 header-social">		
+					<ul >
+						<li><a href="#"><i></i></a></li>
+						<li><a href="#"><i class="ic1"></i></a></li>
+						<li><a href="#"><i class="ic2"></i></a></li>
+						<li><a href="#"><i class="ic3"></i></a></li>
+						<li><a href="#"><i class="ic4"></i></a></li>
+					</ul>
+					
+			</div>
+				<div class="clearfix"> </div>
+		</div>
+		</div>
+		
+		<div class="container">
+		
+			<div class="head-top">
+			
+		 <div class="col-sm-8 col-md-offset-2 h_menu4">
+				<nav class="navbar nav_bottom" role="navigation">
+ 
+ <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header nav_2">
+      <button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+     
+   </div> 
+   <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+        <ul class="nav navbar-nav nav_1">
+            <li><a class="color" href="index.html">Home</a></li>
+            
+    		<li class="dropdown mega-dropdown active">
+			    <a class="color1" href="#" class="dropdown-toggle" data-toggle="dropdown">Women<span class="caret"></span></a>				
+				<div class="dropdown-menu ">
+                    <div class="menu-top">
+						<div class="col1">
+							<div class="h_nav">
+								<h4>Submenu1</h4>
+									<ul>
+										<li><a href="product.html">Accessories</a></li>
+										<li><a href="product.html">Bags</a></li>
+										<li><a href="product.html">Caps & Hats</a></li>
+										<li><a href="product.html">Hoodies & Sweatshirts</a></li>
+										
+									</ul>	
+							</div>							
+						</div>
+					</div>                  
+				</div>				
+			</li>
+            <li ><a class="color6" href="contact.html">Contact</a></li>
+        </ul>
+     </div><!-- /.navbar-collapse -->
+
+</nav>
+			</div>
+			<div class="col-sm-2 search-right">
+				<ul class="heart">
+				<li>
+				<a href="wishlist.html" >
+				<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+				</a></li>
+				<li><a class="play-icon popup-with-zoom-anim" href="#small-dialog"><i class="glyphicon glyphicon-search"> </i></a></li>
+					</ul>
+					<div class="cart box_1">
+						<a href="checkout.html">
+						<h3> <div class="total">
+							<span class="simpleCart_total"></span></div>
+							<img src="{{ asset('images/cart.png') }}" alt=""/></h3>
+						</a>
+						<p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+
+					</div>
+					<div class="clearfix"> </div>
+					
+						<!----->
+
+						<!---pop-up-box---->					  
+			<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all"/>
+			<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
+			<!---//pop-up-box---->
+				
+						<!----->
+			</div>
+			<div class="clearfix"></div>
+		</div>	
+	</div>	
+</div>
+<!--banner-->
+<div class="banner">
+<div class="container">
+<div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
@@ -40,13 +165,9 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
+                            
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
+                                
                             @endif
                         @else
                             <li class="nav-item dropdown">
@@ -76,5 +197,8 @@
             @yield('content')
         </main>
     </div>
+			</div>
+</div>
+    
 </body>
 </html>
